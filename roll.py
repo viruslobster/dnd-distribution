81c0bc606980894c42dd17c27e3b8ea56eb21837
Michael Stevens <msteven9@fbcom>
2023-07-10 22:38:24 -0400

    first commit

---
+++ roll.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from random import randint
+
+rolls = sorted(randint(1, 6) for _ in range(4))
+print(rolls)
+print(sum(rolls[1:]))
+#$
+[1, 4, 4, 6]
+14
+#~
+
+CHA - 18
+CON - 14
+DEX - 14
+WIS - 12
+INT - 12
+STR - 14
+
